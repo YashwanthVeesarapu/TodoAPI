f68a28e764c0427ab22dbbf0b3c9a5d891199945
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2024-05-30 10:12:10 -0400

    change deployment

--- src/main/java/us/redsols/todo/TodoApplication.java
+++ src/main/java/us/redsols/todo/TodoApplication.java
@@ -3,10 +3,12 @@ package us.redsols.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan(basePackages = "us.redsols.todo")
 @EnableScheduling
 public class TodoApplication {
 	public static void main(String[] args) {
